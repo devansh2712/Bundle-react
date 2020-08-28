d336589e8b83b77ae4a6a5bbade049c2c0689404
Aryan Pandey <aryanappandey@gmail.com>
2020-08-29 00:22:35 +0530

    project code: get balance function update
    https://smartpy.io/dev/index.html?code=eJy1Vk1r3DAQve@vEM5hZbo1TXsrGJoGAi2ElnTvRmtrEyW25Epyypb@@I7Gsi2tvUmgjQ7G0rwZjd58SKJplbbENEzb9kCYIaZdlTUzhmzFvdKiaLUqSmra7FJJq1lp048rAqPie1IIKWxBDa_3G1Y1QnqZG24xc3I6LrkB5qqutDkYlEryTSRUvyTXyyI0f1FVmhuT4ySWa_5z38nKLGs7Ea9OWe6NLgprIR@WJaVqmmWJ5o_qgefkitXmSPTI6o4va@1YzWR5Qlgyy2@VPgzSFKX4@QRLHCJzKFolpB1jY7hFOvvotEyzxoThaTNAFPbQ8qMAITLeHsDbG14qXcVYN8aYbX10IkS6mv8B9pFrsT9QdAIio_Mc06VilmVhpNM4nWbyvPc2QydWT_JxC6dF@pGQmIljd0ieBxui8cmTIYOFbDtL1366ngBjKnoEzDuheYVZaAIgJtdk5p6XFtcCCGaZh7j_TkoBySCUnCGHFBnQfg6QEbN6niKfhsckBdnpfwM@wAb1qy9LzBuskNfOTF@HDvJZqfq183Ioe5@S_fRpJlrY5MolxdBC0fiGsEZ10gacnPUrufPR8t_0_N3kwVlImOco5GWvVVOEBbohVhUkXhkb0_a6gw3SrGYHBWlEE1R3F4P7STaEJk4b5lbBLEHFJIUxOtT7StDZWpWsput@aY1@@cMt0H6qACPmyZ8okHQWgHyrO07SNE4oWdEjejN0PT1OcRLu3N8b5G2kMsYUDBaWG0sla5xe8t0XcemvyiQdY424MMtLLpkWiviYGlsMSzSdobK7c3rKOlLuGApMsbJ0_tLkwgmEAbxVOlSoRX_UmYITBMCd2sF3AXijdlyHPtyrO7mI_AqCANeIh8Wdr2E9gJ19gbeDAD9_8_HEJmg66hKMRG8UJKF_G2RsVqcjk@9pIkbT4YYt13ulm_HmXNb9wW1_5yVz4@RNjp4taH6gyTenBZVjxK3k1RPq2Xh395crBms8UqY7SX176h9di0cgQQOaHWKSP@OG79S@tFxlxfs7z7B7iCrH984S43D23gAxFm4lVw8iZmBy3LXE_qI84fkE@EfXY@rmFqbmzMJn5xCHjW_JU0d@iXkkA72HGEm7RAHcwuT4jo0o8ML_evyzmYnwMXDiHEPHHOP5F4nHqEs-

--- contract/projectfile.py
+++ contract/projectfile.py
@@ -1,65 +1,95 @@
-# Project contract
 import smartpy as sp
-
 class Tijori_pro_c(sp.Contract):
-    def __init__(self,admin):
+    def _init_(self,admin):
         self.init(
             product=sp.none,
             owner=sp.none,
-            admin=admin,
+            adminAddress=admin,
             reqfunds=sp.none,
             funded=sp.none,
             address=sp.none,
             link=sp.none,
             comm=sp.none,
             revoke= False,
+            value=sp.none,
             balance=sp.none,
             category=sp.none)
-            
+    
     @sp.entry_point
     def setowner(self,params):
-        sp.set_type(params, sp.TAddress)
-        sp.verify(sp.sender == self.data.address)
-        self.data.admin=params
+        sp.set_type(
+            params,
+            sp.TRecord(
+                owner=sp.TAddress
+            )
+        )
+        sp.verify(sp.sender==self.data.adminAddress)
+        self.data.adminAddress=params.owner
+
+    @sp.entry_point
+    def get_value(self):
+        sp.verify(sp.sender == self.data.owner)
+        product=input('product')
+        reqfunds=input('required funds')
+        link=input('project link')
+        comm=input('communnication link')
+        category=input('category')
+
+        
 
 
+    @sp.entry_point
+    def get_balance(self):
+        balance=sp.balance
+        print(balance)
+    
     @sp.entry_point
     def setRevoke(self,params):
-        sp.set_type(params, sp.TBool)
-        sp.verify(sp.sender==self.data.admin)
-        self.data.revoke=params
-        
+        sp.set_type(
+            params,
+            sp.TRecord(
+                revoke=sp.TBool
+            )
+        )
+        sp.verify(sp.sender==self.data.adminAddress)
+        self.data.revoke= params.revoke
     @sp.entry_point
-    def spendFunds(self,params):
-        sp.set_type(params, sp.TRecord(from_=sp.TAddress, to_ =sp.TAddress, value=sp.TNat).layout(("from_ as from", ("to_ as to", "value"))))
-        sp.verify((sp.sender == self.data.admin) |
-            (~self.data.revoke &
-                   ((params.amount<= self.data.funded) |
-                   (sp.send(sp.TAddress,amount))
-                   (_balance = self.data.funded - params.amount))))
-    
-    
+    def spendFunds(self,address, amount):
+        #amount= sp.tez(10)
+        #sp.set_type(params, sp.TRecord(from_=sp.TAddress, to_ =sp.TAddress, value=sp.TMutez).layout(("from_ as from", ("to_ as to", "value"))))
+        amount = sp.local('amount', sp.amount)
+        sp.verify((sp.sender == self.data.adminAddress) |
+            (self.data.revoke==True ))
+        sp.send(address, amount.value)
+        balance = self.data.funded - amount.value
     @sp.add_test(name = "Project contract")
     def test():
-
         scenario = sp.test_scenario()
         scenario.h1("Project contract")
-
-
         admin = sp.test_account("Administrator")
         alice = sp.test_account("Alice")
         bob   = sp.test_account("Robert")
         john  = sp.test_account("John")
-        mike = sp.test_account("Mike") 
-        
+        mike = sp.test_account("Mike")
         #Initialize contracts
-        proC = Tijori_pro_c(admin.address)
-        scenario.h1("Project Contract")
-        
+        proC = Tijori_pro_c(admin=admin.address)
+        scenario.h2("initialize")
         #perform setowner
         scenario.h2("Set owner")
-        scenario += proC.setowner(owner = ownerC.address).run(sender=admin)
-        
-        
+        scenario += proC
+        scenario.h3("Owner assigned")
+        scenario += proC.setowner(owner=alice.address).run(sender=admin)
+        #perform set revoke
         scenario.h2("set revoke")
-        scenario += proC.setRevoke(revoke = False).run(sender=admin)
+        scenario += proC.setRevoke(revoke=True).run(sender=alice, valid=False)
+        scenario.h3("revoke state defined")
+        #perform spend funds
+        scenario.h2("spend funds")
+        scenario += proC.setRevoke(revoke=True).run(sender=admin)
+        scenario += proC.spendFunds(address=admin.address,amount=sp.tez(10)).run(sender=admin)
+        scenario.h3("funds sent")
+        #perform get balance
+        scenario.h2("balance")
+        scenario += proC.setRevoke(revoke=True).run(sender=admin)
+        #scenario += proC.get_balance()
+        scenario.h3("balance defined")
