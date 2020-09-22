272617e9388f2b9e16fc3779d86876756cc4fe99
Aryan Pandey <aryanappandey@gmail.com>
2020-09-22 20:11:03 +0530

    updated contract
    https://smartpy.io/dev/index.html?code=eJztW91v4zYSf47_Cp4XaGSsKtjO9h4Wp@LSLhZI79otuulTEAiMRcdqZEkQ6ey51_Zv7_BTpETJcpJei4P1kLXI4XDmN8P5oL3ZtiprhugW16zaI0wRrSaTVY4pRe8uPyTX5QMpAlpFX5cFq_GKzd5OEDwpWaMkyYqMJUlASb4OEU63WaGm@cNHI04RVHhHSYpi9B7nlIToDue4WBEKI8D5LrtPtrgK2CPOd0SOXf9AVmWdBriq6hLGFen1t0DH_71M05pQGorB7zCbGaaKkI_NlEwZBclZWcOUeA8RKxnOP@6qKt_D4Hw2EUL_ExYS0HKfVGVWMKMnrC7omtRKzwrXeEttRauIEpawfUUCORnaOqzrcpsosYzcrOwM2epz8aMc78sdC4Kp5AC24R@mIQqmfDm8sxLepmLhdAaPLdIjqbP1PgiEdEVKQP1Y2iTFDEcOMjP0i1nKn@C3hlAZ7zOHQBApZSOlYIzMVm1@kr7hqR3gxuZwGxlr3xhOt@jLWCEeCT1dNTlHnKYKw6v1dwS4UlzvtWwAlA@Vg6Job2rv7m49hoOABdOkwGz8tp8fvSso2ix_7UptA5CtkWu3vzl2@8w2k3t6HMK3jn2fZtmjkPFyaMM0eIwlB_LEU0wrdYjGHlq1wDmgHk_snLSGCOc1wen@UsrN46cHpQaNBqLonjBtZCVGyKOcJzp09uDhEP1ifETpx0dBkR8LitfkMs_LT3z3rze4uAfFhtzTJ9@NKxv3BMddB80IRvqeA3WsHb8qy9wbCsbER4@GJqnJTQ4KfWkzPFZ45W8vKX87NY5RA1awJ54fLDU4dH5eSMPBnIA7YPYGH0U6IrIaFnZt0aYeBPduVxd_eXBHZNAOZk_IoR0e43JFZ9nhPOqWgr5NbApvwhHJxetwujCW_uaYE_Lwbx4AohXU2TgraNBd1QOZVlqKX0uPaHCbh8iuov_7q5UmHzPyKdDeYXSB7PGVXD7gjrDlLh@whbHBmO1MRnnGhlH5qRjMMmMEuW5sfexR_BE6ndmgwJYj@WRxYrwG5jlJ47BEPad80vxRreD3dfkTWcEep3bw1A6e2sFTO3hqB0_t4KkdPLWDp3bw1A6e2sFTO3hqB__P20H3g@wKK9kVjuoHwRzeZtBxZ2GzmJO6e_bmUW7L9a5IqdwEm36qxnhb7gpm7WnG5MnIyxXOg3M5dC4gUks84c2slSf9HzFvVLVDO8AXJvxaUrTjTS@i7y4_jEJTNq5gKVLcs00ILwmPFHV2t2NZOQJpwSCWbNyIonjGhrkznWa02jESz91h8IR0t2JJBhVQd6oqKc5hrj3VFjtuD7T2xqW4f@DgF2VBOvtwZxygALtsyfaO1NzZW5cND2TfSpedjvXs7Ex_9N1NdDtcs@zM1kmtuipYdwtriXt_AdSTM_34lswmclxSzEJODfoqULoKo87eFgRcNg_BWKUpnBmcF2U8qCeIBgLGg5Dz57FkNgpdxZ23jsWF@2Ga_28hkE4Pseygejx6Capv4Vj97KcZB@hxQHneJj3xaaCLSfSZlGFJ_FqkJxWJuWe1Lwf6ZS1KlNGEllsStG@GHCqJlKCTUh9U1Q4wfwF1bXGGVbYp_WofNLUJnVLxnlzzCtSE8RVDD7guMNrgDMllaCUSLyVerJyQrx24kawVPe1XD0QO9Zc2qp0saVaDkInTNEmjdM@DqtRtS1k5panWLYa9Fbuz9MZacRs3hbuTHh2cVIKI_RrxRxhclxJSr5V6Cw53i7AatrDaxd505Tye81hClabccxyPKb9YmM7shc66VyBScwtvsPJY0DS9jlf0iPGKr4MgHMx7CRxIh0OGzD28KhJnxnID_xHV9dPrGC0MRZVC2ZeS_ziXfQ3xOAe1ioDGQzXnIfe0Ft5oerevNJlJT4cqC83DJr_ry@SJkXQYM1EuykDDs2NW3FtCqpHxNbwNmy5EWyBTP8ia@jko01EomwrlRi_og5l6YDaVhvJ5UVIonOzOYxB6ztD0cxx6eFFbCQ4@9xUojABBcWqD0F3uC6V9XzIMe6va8jbier2O3ds573IrFluX1CrKfu7hMAZOy5WfiqfxjT8DUMsx_0BE@0BcZwXOM323_6c3952rTt0Mx_NjLwH6NFYc1dVO21O6OzcYyj9g_ykj2yrHjNApKkqGsgIlSYG3JEnemr1BugQoWMAn4uk1fEx0bTBtrpz0ECeFYBLYsqxIgetM9CR8OtEDVq4WZaw0iCDBK1EGBufOPdb5cxZsM@alh_HzmXW20k29e_RRvuMTDimu99gvBJ9wuZJHqEI2Xr5yyiF_KLe6onKJ_8UnLLXoJnvY4NpH@lFOOXx_wrh4zHzU34gZi7OqHRY@Yj3XJV8OkC@75BcD5BcWOVSIq5hfeQm76@v6cDEPl3OH6SrWqUkh3v3SJhWlmWCn_tOCw9Ripwjdn7T0EGunjjaLYHp99c2HH66QqajVqZj6yJfB9N_gr6hcCzKkYKA@YropPwU36rco3KND6a6h8MRQqRwK7wmVa4TS5rc9e@dqb32AvftuLoLp@8tFtJRlbQDQIdkU9kupYDZfrfSw5axa8cTHDOx_iJMy0whu3E8GuS273AZs@Jr73GokG37BChVa9jNJ@9zhI7@rFuWrzxhyt8i60FaaxMrg5nTIZMLD7pbfGAXL@Xw2i@odv5PgGTbW_tIQcrLFnNN1RascyRDdrfi3Vetd7hGSl83cZj02xyW6OojD@UfC@DW39Lnz_k0i53rJNq6trf4RV4_38M2a3ZR2fuWOF865EHqGgNqbxsk4hQ5d3ar4TaRPlbqwgVDqeIeMLFatJJ3I4xtdXm9cXjIGaUfTvN6M47X4wuElo1uLFdCIKihL1Y8Fe1BsIGngQ0P4KdN5D6KWU3fvjpg6Rz514fKpCy96VMeNNsOH19lG9iWqR2xiyaInlrwQN2Hjl5KM@_EwJhVwGsZE@AM_f2JXaNowTwY@z1EyOm2dbonihezp4i_GSDiK0xuHE68JjmS09It0wJx@XheK1989h7_HAlUb0oNRzUTB0UYQ9eBL2MDP6HgTuHyeZQGX1RMM0EbzIP7vVa_dD7vpxnVlomPhH1WaKEcSeujNe8urGsOs7p_7ldAN9mJ0MHEYcxw70eSoD78Duck8_g--

--- contract/main.py
+++ contract/main.py
@@ -1,5 +1,85 @@
 import smartpy as sp
-class token(sp.Contract):
+
+class DAO_Token(sp.Contract):
+    def __init__(self, admin):
+        self.init(paused = False, balances = sp.big_map(tvalue = sp.TRecord(approvals = sp.TMap(sp.TAddress, sp.TNat), balance = sp.TNat)), administrator = admin, totalSupply = 0)
+
+    @sp.entry_point
+    def transfer(self, params):
+        sp.set_type(params, sp.TRecord(from_ = sp.TAddress, to_ = sp.TAddress, value = sp.TNat).layout(("from_ as from", ("to_ as to", "value"))))
+        sp.verify((sp.sender == self.data.administrator) |
+            (~self.data.paused &
+                ((params.from_ == sp.sender) |
+                 (self.data.balances[params.from_].approvals[sp.sender] >= params.value))))
+        self.addAddressIfNecessary(params.to_)
+        sp.verify(self.data.balances[params.from_].balance >= params.value)
+        self.data.balances[params.from_].balance = sp.as_nat(self.data.balances[params.from_].balance - params.value)
+        self.data.balances[params.to_].balance += params.value
+        sp.if (params.from_ != sp.sender) & (self.data.administrator != sp.sender):
+            self.data.balances[params.from_].approvals[sp.sender] = sp.as_nat(self.data.balances[params.from_].approvals[sp.sender] - params.value)
+
+    @sp.entry_point
+    def approve(self, params):
+        sp.set_type(params, sp.TRecord(spender = sp.TAddress, value = sp.TNat).layout(("spender", "value")))
+        sp.verify(~self.data.paused)
+        alreadyApproved = self.data.balances[sp.sender].approvals.get(params.spender, 0)
+        sp.verify((alreadyApproved == 0) | (params.value == 0), "UnsafeAllowanceChange")
+        self.data.balances[sp.sender].approvals[params.spender] = params.value
+
+    @sp.entry_point
+    def setPause(self, params):
+        sp.set_type(params, sp.TBool)
+        sp.verify(sp.sender == self.data.administrator)
+        self.data.paused = params
+
+    @sp.entry_point
+    def setAdministrator(self, params):
+        sp.set_type(params, sp.TAddress)
+        sp.verify(sp.sender == self.data.administrator)
+        self.data.administrator = params
+
+    @sp.entry_point
+    def mint(self, params):
+        sp.set_type(params, sp.TRecord(address = sp.TAddress, value = sp.TNat))
+        sp.verify(sp.sender == self.data.administrator)
+        self.addAddressIfNecessary(params.address)
+        self.data.balances[params.address].balance += params.value
+        self.data.totalSupply += params.value
+
+    @sp.entry_point
+    def burn(self, params):
+        sp.set_type(params, sp.TRecord(address = sp.TAddress, value = sp.TNat))
+        sp.verify(sp.sender == self.data.administrator)
+        sp.verify(self.data.balances[params.address].balance >= params.value)
+        self.data.balances[params.address].balance = sp.as_nat(self.data.balances[params.address].balance - params.value)
+        self.data.totalSupply = sp.as_nat(self.data.totalSupply - params.value)
+
+    def addAddressIfNecessary(self, address):
+        sp.if ~ self.data.balances.contains(address):
+            self.data.balances[address] = sp.record(balance = 0, approvals = {})
+
+    @sp.view(sp.TNat)
+    def getBalance(self, params):
+        sp.result(self.data.balances[params].balance)
+
+    @sp.view(sp.TNat)
+    def getAllowance(self, params):
+        sp.result(self.data.balances[params.owner].approvals[params.spender])
+
+    @sp.view(sp.TNat)
+    def getTotalSupply(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.totalSupply)
+
+    @sp.view(sp.TAddress)
+    def getAdministrator(self, params):
+        sp.set_type(params, sp.TUnit)
+        sp.result(self.data.administrator)
+        
+    
+    
+
+class Project_token(sp.Contract):
     def __init__(self, admin):
         self.init(paused = False, balances = sp.big_map(tvalue = sp.TRecord(approvals = sp.TMap(sp.TAddress, sp.TNat), balance = sp.TNat)), administrator = admin, totalSupply = 0)
 
@@ -76,235 +156,211 @@ class token(sp.Contract):
         sp.result(self.data.administrator)
         
         
+        
 class project(sp.Contract):
-    def __init__(self,admin):
+    def __init__(self,own):
         self.init(
-            admin=admin,
-            product=sp.none,
-            owner=sp.none,
-            revoke=False,
-            reqfunds=sp.none,
-            funded=sp.none,
-            link=sp.none,
-            comm=sp.none,
-            value=sp.none,
-            balance=sp.none,
-            approval=0,
-            category=sp.none)
-            
-    @sp.entry_point
-    def setowner(self,own):
-        sp.set_type(own, sp.TAddress)
-        sp.verify(sp.sender == self.data.admin)
-        sp.verify(~self.data.owner.is_some())
-        self.data.owner = sp.some(own)
+            owner=own)
         
     @sp.entry_point
-    def getvalue(self,pro,reqf,lin,com,cat):
-        product=pro
-        reqfunds=reqf
-        link=lin
-        comm=com
-        category=cat
-        
-    @sp.entry_point
-    def getbalance(self):
-        balance=sp.balance
-        
-    @sp.entry_point
-    def setrevoke(self,params):
-        sp.set_type(
-            params,
-            sp.TRecord(
-                revoke=sp.TBool
-            )
-        )
-        self.data.revoke= params.revoke
-        
-    @sp.entry_point
-    def spendfunds(self,address, amount):
-        amount = sp.local('amount', sp.amount)
-
-        sp.send(address, amount.value)
-        balance = self.data.funded - amount.value
+    def spendfunds(self,address, traamount):
+        traamount = sp.local('amount', sp.amount)
+        sp.verify(traamount.value<=sp.balance)
+        sp.send(address, traamount.value)
         
         
-    
-
+        
 class DAO(sp.Contract):
     def __init__(self,admin,strength,min_contribution):
         self.init(
             admin=admin,
             strength=strength,
+            dispute=0,
             product_id =0,
-            maxvoteid=0,
-            maxvotecount=0,
+            proposal_id=0,
             min_contribution=min_contribution,
-            token=sp.none,
+            daotoken=sp.none,
+            projecttoken=sp.none,
             addmemberdata = sp.big_map(tkey = sp.TAddress,
                 			        tvalue = sp.TRecord(
-                       				#address = sp.TAddress,
-                       				contribution = sp.TNat,
-                       				status = sp.TBool
+                       				contribution = sp.TInt,
+                       				balance = sp.TInt
 									                    )
 		    						),
 			addprojectdata = sp.big_map( 
                 tkey = sp.TInt, 
                 tvalue = sp.TRecord(
-                    roundno= sp.TNat,
                     serialno=sp.TInt,
                     proadd=sp.TAddress,
                     vote = sp.TInt
                 )
             ),
             addpropoasldata = sp.big_map( 
-                tkey = sp.TNat, 
+                tkey = sp.TInt, 
                 tvalue = sp.TRecord(
-                    starttime= sp.TTimestamp,
-                    endtime= sp.TTimestamp,
+                    proposer=sp.TAddress,
+                    fund=sp.TMutez,
+                    serialno=sp.TInt,
                     vote = sp.TInt
                 )
-            ),
-		    						
-		    						
-		    						
-			
             )
-            
+            )
+    
+
+        
+        
     @sp.entry_point
-    def set_token(self,Token):
+    def set_daotoken(self,Token):
         sp.set_type(Token, sp.TAddress)
         sp.verify(sp.sender == self.data.admin)
-        sp.verify(~self.data.token.is_some())
-        self.data.token = sp.some(Token)
-        
+        sp.verify(~self.data.daotoken.is_some())
+        self.data.daotoken = sp.some(Token)
+    @sp.entry_point
+    def set_projecttoken(self,Token):
+        sp.set_type(Token, sp.TAddress)
+        sp.verify(sp.sender == self.data.admin)
+        sp.verify(~self.data.projecttoken.is_some())
+        self.data.projecttoken = sp.some(Token)
         
     @sp.entry_point
     def addmember(self,contribution):
-        
-        sp.set_type(contribution, sp.TNat)
+        #restrict karna hai member count se
+        sp.set_type(contribution, sp.TInt)
         contribution = contribution
         sp.verify(contribution >= self.data.min_contribution)
         mem_address = sp.sender
         
         sp.if ~self.data.addmemberdata.contains(mem_address):
-            self.data.addmemberdata[mem_address]=sp.record(contribution=contribution,status=True)
-            
-        
-        
-
-        
-
-    
-    
+            self.data.addmemberdata[mem_address]=sp.record(contribution=contribution,balance=contribution)
+            tokenContract = sp.contract(sp.TRecord(address = sp.TAddress, tokbal = sp.TNat),
+                                    self.data.daotoken.open_some(),
+                                    "mint").open_some()
+            #sp.transfer(sp.record(address = sp.sender, value =contribution ),
+                    # sp.tez(0),
+                    # tokenContract)
     @sp.entry_point
-    def addproduct(self,rou):
+    def addproduct(self):
         
-        sp.set_type(rou, sp.TNat)
         self.data.product_id += 1
         pd_index = self.data.product_id
         
         sp.if ~self.data.addprojectdata.contains(pd_index):
-            self.data.addprojectdata[pd_index] = sp.record(roundno = 0,serialno=pd_index,vote = 0,proadd=sp.sender)
-            
-            
-    '''def addproposal(self,st,et):
-        
-        sp.set_type(st, sp.TTimestamp)
-        sp.set_type(et, sp.TTimestamp)
-        
-        self.data.addpropoasldata[approval].starttime=st
-        self.data.addpropoasldata[approval].endtime=et
-        approval+=1
-        set params'''
-            
-    '''@sp.entery_point
-    def voteproject(self,proid,votetype):
-        sp.set_type(proid)'''
-        
-
+            self.data.addprojectdata[pd_index] = sp.record(serialno=pd_index,vote = 0,proadd=sp.sender) 
+    
     @sp.entry_point
-    def vote(self,proindex):
+    def addproposal(self,funding):
+        funding = sp.local('amount', sp.amount)
+        self.data.proposal_id += 1
+        ps_index = self.data.proposal_id
+        
+        sp.if ~self.data.addprojectdata.contains(ps_index):
+            self.data.addpropoasldata[ps_index] = sp.record(serialno=ps_index,vote = 0,proposer=sp.sender,fund=funding.value)
+    
+    @sp.entry_point
+    def voteproject(self,proindex,value):
         
         sp.if self.data.addprojectdata.contains(proindex):
-            self.data.addprojectdata[proindex].vote+=1
-              
-    @sp.entry_point
-    def voteresult(self):
-        r=self.data.product_id
-        self.data.maxvotecount=self.data.addprojectdata[1].vote
-        sp.for x in sp.range(1,r,1):
-            sp.if (self.data.addprojectdata[x].vote>self.data.maxvotecount):
-                self.data.maxvoteid=self.data.addprojectdata[x].serialno
-                self.data.maxvotecount=self.data.addprojectdata[x].vote
-                
-        
+            sp.if self.data.addmemberdata.contains(sp.sender):
+                self.data.addprojectdata[proindex].vote+=value
+                self.data.addmemberdata[sp.sender].balance-=value
             
-@sp.add_test(name="Test_contract")
-def testContract():
-    scenario = sp.test_scenario()
-    admin = sp.test_account('Administrator')
-    amit = sp.test_account('Amit')  
-    dhruv = sp.test_account('Dhruv')  
-    aryan = sp.test_account('Aryan')  
-    devansh = sp.test_account('Devansh')  
-    komal = sp.test_account('Komal')
-    shikhar = sp.test_account('Shikhar')  
-    jaanvi = sp.test_account('Jaanvi')
-    daoadd= sp.test_account('daoaccount')
-    proadd =sp.test_account('projectaccount')
-    daoc=DAO(admin.address,10,10)
-    tokc=token(daoc.address)
-    proc=project(daoadd.address)
-    scenario.h1("TIJORI Contract testing")
-    scenario.h2("List of test accounts")
-    scenario.show([admin, amit, dhruv,aryan,devansh,komal,shikhar,jaanvi])
-    scenario.h2("list of contracts")
-    scenario.h3("FA1.2 token(DAO Token)")
-    scenario.show([tokc.address])
-    scenario.h3("DAO contract")
-    scenario.show([daoc.address])
-    scenario.h3("Project contract")
-    scenario.show([proc.address])
-    scenario.h2("Project contract testing")
-    scenario+=proc
-    scenario.h2("Set owner")
-    scenario +=proc.setowner(devansh.address).run(sender= daoadd)
-    scenario.h3("set owner succesful")
-    scenario.h2("Get value")
-    scenario+=proc.getvalue(pro='a',reqf=5000,lin='c',cat='d').run(sender=devansh)
-    scenario.h3("get value succesful")
-    scenario.h2("get balance")
-    scenario+=proc.getbalance().run(sender=devansh)
-    scenario.h3("get balance sucesful")
-    scenario.h2("setrevoke")
-    scenario+=proc.setrevoke(revoke=True).run(sender=devansh)
-    scenario.h3("set revoke sucessful")
-    scenario.h2("spend funds")
-    scenario+=proc.spendfunds(address=shikhar.address,amount=sp.tez(10)).run(sender=devansh)
-    scenario.h2("DAO Contract testing")
-    scenario += daoc
-    scenario.h3("Dao Initialized")
-    scenario.h2('Set token')
-    scenario += daoc.set_token(proc.address).run(sender = admin)
-    scenario.h3("Set token succesful")
-    scenario.h2("Add member")
-    scenario +=daoc.addmember(10).run(sender=dhruv)
-    scenario +=daoc.addmember(10).run(sender=amit)
-    scenario +=daoc.addmember(10).run(sender=devansh)
-    scenario.h3("Add member succesful ")
-    scenario.h2("Add project")
-    scenario+=daoc.addproduct(1).run(sender=proadd)
-    scenario.h3("add project successful")
-    scenario.h2("vote")
-    scenario+=daoc.vote(1).run(sender=dhruv)
-    scenario+=daoc.vote(1).run(sender=amit)
-    scenario+=daoc.vote(1).run(sender=devansh)
-    scenario+=daoc.vote(1).run(sender=jaanvi)
-    scenario.h3("voting done")
-    scenario.h2("show voting result")
-    scenario+=daoc.voteresult().run(sender=devansh)
-    scenario.h3("voating results calculated")
-    #scenario.h2("add proposal data")
-    #scenario+=daoc.addproposal(st=sp.timestamp(100000),et=sp.timestamp(110000))
-    #scenario.h3("add propoasl sucsesful")
+    @sp.entry_point
+    def voteproposal(self,proindex,value):
+        
+        sp.if self.data.addpropoasldata.contains(proindex):
+            sp.if self.data.addmemberdata.contains(sp.sender):
+                self.data.addpropoasldata[proindex].vote+=value
+                self.data.addmemberdata[sp.sender].balance-=value
+    @sp.entry_point
+    def finalise(self,address, traamount):
+        traamount = sp.local('amount', sp.amount)
+        sp.verify(traamount.value<=sp.balance)
+        sp.verify(self.data.dispute==0)
+        sp.send(address, traamount.value)
+    @sp.entry_point
+    def dispute(self, value):
+        self.data.dispute=value
+    
+    
+if "templates" not in __name__:
+    @sp.add_test(name="Test_contract")
+    def contracttesting():
+        scenario=sp.test_scenario()
+        admin = sp.test_account('Administrator')
+        admin = sp.test_account('Administrator')
+        amit = sp.test_account('Amit')  
+        dhruv = sp.test_account('Dhruv')  
+        aryan = sp.test_account('Aryan')  
+        devansh = sp.test_account('Devansh')  
+        komal = sp.test_account('Komal')
+        shikhar = sp.test_account('Shikhar')  
+        jaanvi = sp.test_account('Jaanvi')
+        product1 = sp.test_account('product1')
+        product2 = sp.test_account('product2')
+        product3 = sp.test_account('product3')
+        daoc=DAO(admin.address,10,20)
+        proc=project(devansh.address)
+        dtokenc=DAO_Token(admin.address)
+        ptokenc=Project_token(admin.address)
+        scenario.h1("TIJORI Contract testing")
+        scenario.h2("List of test accounts")
+        scenario.show([admin, amit, dhruv,aryan,devansh,komal,shikhar,jaanvi])
+        scenario.h2("list of contracts")
+        scenario.h3("FA1.2 token(DAO Token)")
+        scenario.show([dtokenc.address])
+        scenario.h3("DAO contract")
+        scenario.show([daoc.address])
+        scenario.h3("Project contract")
+        scenario.show([proc.address])
+        scenario.h2("Project contract testing")
+        scenario+=proc
+        scenario.h2("Project contract initialized")
+        scenario.h2("Spend funds")
+        scenario+=proc.spendfunds(address=shikhar.address,amount=sp.mutez(200)).run(sender=devansh,amount=sp.tez(1000))
+        scenario.p("Spend funds successful")
+        scenario += daoc
+        scenario.h3("Dao Initialized")
+        scenario.h2('Set DAO token')
+        scenario += daoc.set_daotoken(proc.address).run(sender = admin)
+        scenario.h3("Set  DAO token succesful")
+        scenario.h2('Set DAO token')
+        scenario += daoc.set_projecttoken(proc.address).run(sender = admin)
+        scenario.h3("Set Project token succesful")
+        scenario.h2("Add member")
+        scenario +=daoc.addmember(20).run(sender=dhruv,amount = sp.tez(20))
+        scenario +=daoc.addmember(40).run(sender=jaanvi,amount= sp.tez(40))
+        scenario +=daoc.addmember(15).run(sender=komal,amount= sp.tez(15), valid= False)
+        scenario.h3("Add member succesful ")
+        scenario.h2("Add project")
+        scenario+=daoc.addproduct().run(sender=product1)
+        scenario+=daoc.addproduct().run(sender=product2)
+        scenario+=daoc.addproduct().run(sender=product3)
+        scenario.h3("add project successful")
+        scenario+=daoc.addproposal(funding=sp.mutez(100)).run(sender=devansh)
+        scenario+=daoc.addproposal(funding=sp.mutez(100)).run(sender=komal)
+        scenario+=daoc.addproposal(funding=sp.mutez(100)).run(sender=dhruv)
+        scenario.h3("add propsal successful")
+        scenario.h2(" Proposal voating ")
+        scenario+=daoc.voteproposal(proindex=1,value=5).run(sender=dhruv)
+        scenario+=daoc.voteproposal(proindex=1,value=4).run(sender=amit)
+        scenario+=daoc.voteproposal(proindex=2,value=5).run(sender=devansh)
+        scenario+=daoc.voteproposal(proindex=3,value=6).run(sender=jaanvi)
+        scenario.h3("proposal voating succesful")
+        scenario.h2(" Project voating ")
+        scenario+=daoc.voteproject(proindex=1,value=5).run(sender=dhruv)
+        scenario+=daoc.voteproject(proindex=1,value=4).run(sender=amit)
+        scenario+=daoc.voteproject(proindex=2,value=5).run(sender=devansh)
+        scenario+=daoc.voteproject(proindex=3,value=6).run(sender=jaanvi)
+        scenario.h3("Project voating succesful")
+        scenario.h2("Finalise")
+        scenario+=daoc.finalise(address=product1.address,amount=sp.mutez(200)).run(sender=devansh,amount=sp.tez(1000))
+        scenario.h3("project finalised")
+        scenario.h2("raise dispute")
+        scenario+=daoc.dispute(1).run(sender=dhruv)
+        scenario.h3("raise dispute sucessful")
+        
+        
+        
+        
+        
+        
